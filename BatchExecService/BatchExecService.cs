f7cf879918d0a9bfe1bb94fc567ff01dc5179064
Chen Weiguang <chen.weiguang@gmail.com>
2017-04-30 20:02:10 +0800

    Initial commit and able to run batch execution via .NET Windows Service, loading via TOML format. The compiled executable (service) can be run using sc create.

---
+++ BatchExecService/BatchExecService.cs
@@ -0,0 +1,101 @@
+﻿using Nett;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BatchExecService
+{
+    public partial class BatchExecService : ServiceBase
+    {
+        public BatchExecService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Environment.FailFast("Invalid usage, expected: service <path to toml config>");
+            }
+
+            var configFilePath = args[0];
+            var config = Toml.ReadFile<Config>(configFilePath);
+
+            processesMtx.WaitOne();
+
+            processes = config.CmdGroups
+                .Select(cmdGroup =>
+                {
+                    var startInfo = new ProcessStartInfo
+                    {
+                        WindowStyle = cmdGroup.WindowStyle,
+                        FileName = cmdGroup.FileName,
+                        Arguments = cmdGroup.Arguments,
+                    };
+
+                    var process = new Process
+                    {
+                        StartInfo = startInfo,
+                    };
+
+                    process.Start();
+                    return process;
+                })
+                .ToArray();
+
+            processesMtx.ReleaseMutex();
+
+            // asynchronously wait for all exit codes
+            //exitCodes = WaitAll();
+        }
+
+        protected override void OnStop()
+        {
+            processesMtx.WaitOne();
+
+            if (processes != null)
+            {
+                foreach (var process in processes)
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+
+            processesMtx.ReleaseMutex();
+        }
+
+        //private Task<IEnumerable<int>> WaitAll()
+        //{
+        //    return Task.Run<IEnumerable<int>>(() =>
+        //    {
+        //        processesMtx.WaitOne();
+
+        //        var exitCodes = processes
+        //            .Select(process =>
+        //            {
+        //                process.WaitForExit();
+        //                return process.ExitCode;
+        //            })
+        //            .ToArray();
+
+        //        processesMtx.ReleaseMutex();
+
+        //        Stop();
+        //        return exitCodes;
+        //    });
+        //}
+
+        private Mutex processesMtx = new Mutex();
+        private IEnumerable<Process> processes = null;
+        //private Task<IEnumerable<int>> exitCodes = null;
+    }
+}
---
+++ BatchExecService/Config.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BatchExecService
+{
+    public class Config
+    {
+        public List<CmdGroup> CmdGroups { get; set; }
+    }
+
+    public class CmdGroup
+    {
+        public string FileName { get; set; }
+        public string Arguments { get; set; }
+        public ProcessWindowStyle WindowStyle { get; set; }
+    }
+}
